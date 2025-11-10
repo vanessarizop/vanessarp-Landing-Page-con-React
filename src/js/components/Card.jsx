cca795438d5fc45ef8d3882b351308db944eaa40
vanessarizop <vanessa.rizop@gmail.com>
2025-11-10 17:27:34 +0000

    Se crea React

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -4,12 +4,12 @@ export const Card= (props) =>{
     return(
      <>
      
-<div className="card" >
+<div className="card d-inline-flex m-3 " style={{ width: "20rem", height:"40rem" }} >
   <img src={props.img} className="card-img-top img-fluid" alt="..."/>
    <div className="card-body">
     <h5 className="card-title">{props.nombre}</h5>
-    <p className="card-text text-justify">{props.info}</p>
-    <a href={props.url} className="btn btn-primary">{props.boton}</a>
+    <p className="card-text text-justify  " >{props.info}</p>
+    <a href={props.url}  className="btn btn-primary text-center d-grid mx-4 ">{props.boton}</a>
   </div>
 </div>
 
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -11,14 +11,14 @@ import { Jumbotron } from "./Jumbotron.jsx";
 let viajes = [
 	{
 		nombre: "Destinos Destacados",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate tempus accumsan. Vestibulum posuere tempus sapien quis auctor. .Integer sed turpis sed nulla. Vestibulum posuere tempus sapien quis auctor.",
+		info:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis aliquam nemo, harum aut ea praesentium neque porro ullam odit inventore, impedit, sit excepturi quidem nobis dolores sint maiores veritatis. Amet ex quas laborum dolor.!",
 		img: "https://content-viajes.nationalgeographic.com.es/medio/2017/04/24/bali-indonesia_5ae3dbfe.jpg",
 		url: "https://viajes.nationalgeographic.com.es/a/destinos-mas-populares_11415",
 		boton: "Ir a Destinos"
 	},
 	{
 		nombre: "Las Mejores Ciudades para Visitar en Navidad",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate tempus accumsan. Vestibulum posuere tempus sapien quis auctor. Integer sed turpis sed nulla Integer . ",
+		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate tempus accumsan. Vestibulum posuere tempus sapien quis auctor. Integer sed turpis sed nulla Integer . Integer sed turpis sed nulla Integer .",
 		img: "https://hips.hearstapps.com/hmg-prod/images/dresden-alemania-navidad-1544180714.jpg?crop=1xw:1xh;center,top",
 		url: "https://www.elledecor.com/es/hoteles-restaurantes-planes/g25433209/navidad-destinos-ciudades-viajes/",
 		boton: " Conocer ciudades"
@@ -32,7 +32,7 @@ let viajes = [
 	},
 	{
 		nombre: "Mejores Destinos Turísticos en Verano",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate tempus accumsan. Vestibulum posuere tempus sapien quis auctor. Integer sed turpis sed nulla Integer .",
+		info:" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum a vero aut doloremque quidem perspiciatis porro aspernatur nisi sapiente sequi. Amet consectetur adipisicing elit. Suspendisse vulputate tempus accumsan.",
 		img: "https://travel.usnews.com/dims4/USNEWS/c9e626c/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/format/webp/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2Fgettyimages-177187495_45F9W7n.jpg",
 		url: "https://travel.usnews.com/rankings/best-summer-vacations/",
 		boton: "Mejores Destinos en verano"
